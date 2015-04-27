0e6b366db539f7ff1d1b739b3c1ab49ab3f6a908
Farhadur Rahim <varendrasoft@gmail.com>
2015-04-28 01:14:06 +0600

    Create Cloud.js

---
+++ Cloud.js
@@ -0,0 +1,16 @@
+/*
+ * This is a JavaScript Library Which works for Cloud File Sharing/Store.
+ *
+ * You can use this library in your any number of projects as well as domain whether it is commercial or non-commercial
+ * You can put my name as a contributor if you wish. Reward Should be given, not for taken!!!,
+ * Author: Farhadur Rahim,
+ * Email:  webmechanicx@gmail.com
+ ***************************************************************************************************************************/
+
+var Save;
+var options = {};
+var store = new Array();
+var placeholder; // Work as a DOM Selector;
+Saveto = function(store){
+         return rtn = "Hello Mate"
+};
